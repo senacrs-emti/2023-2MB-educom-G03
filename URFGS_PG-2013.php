e8fe5d337f916ff0c1b3cbeb7a263433ec40cd19
Framezinho <Gabrielframe25@gmail.com>
2023-11-09 11:14:14 -0300

    Transformação em Pagina unica atualizada com SQL
    + Deletadas paginas extras selecionadas por ano.

--- URFGS_PG-2013.php
+++ URFGS_PG-2013.php
@@ -1,3 +1,14 @@
+<?php
+
+$ano = $_GET['ano'];
+$vest = $_GET['vest'];
+
+$sql = "SELECT * FROM livros WHERE edicao LIKE '%$ano%'";
+
+echo $sql;
+$tituloLivro = 'Livro Muito Legal';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -23,7 +34,7 @@
 <main>
     <section class="page">
        <main>
-        <h1>2013</h1>
+        <h1><?php echo $ano;?></h1>
 <div class="card1">
         <div class="card mb-3" style="max-width: 1180px;">
             <div class="row no-gutters">
@@ -32,7 +43,7 @@
               </div>
               <div class="col-md-8">
                 <div class="card-body">
-                  <h5 class="card-title">TITULO</h5>
+                  <h5 class="card-title"><?php echo $tituloLivro;?></h5>
                   <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. At, placeat. Dolor rem sit sunt ipsum nisi? Vero, tenetur. Quasi vitae dolorum, cumque animi error cupiditate sequi fuga recusandae. Reprehenderit, explicabo. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid facilis, sint fugiat possimus ab libero necessitatibus quaerat voluptatem consequatur rerum, sed blanditiis nihil. Obcaecati inventore facere enim illum rerum quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus dolorum eaque accusamus hic maxime. Perspiciatis adipisci repellat molestias molestiae debitis accusamus, at qui veniam, ipsa voluptas, hic porro? Fugiat, dolores? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus nulla, aperiam blanditiis nesciunt provident magnam repellat dolor eaque possimus debitis veniam esse voluptas nam, voluptatem cumque, quod ex tempora quis.</p>
                   <p class="card-text"><small class="text-muted">*GENEROS*</small></p>
                   <p class="card-text"><small class="text-muted">Editora</small></p>
@@ -244,3 +255,4 @@
 </main>
 </body>
 </html>
+?>
