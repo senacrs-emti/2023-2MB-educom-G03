4b13e8e3de68105ecb802ae1c169d81dbf169bcc
adrielyrossetti <108465597+nathaliaalencastro@users.noreply.github.com>
2023-11-14 10:54:23 -0300

    conexao

--- conexao.php
+++ conexao.php
@@ -3,10 +3,13 @@
 $conexao = mysqli_connect('localhost','root','','leituras');
 
 
+// Define DB Params
+define("DB_HOST", ""); // ex. localhost
+define("DB_USER", "");
+define("DB_PASS", "");
+define("DB_NAME", "");
 
-
-
-
-
+// Define URL
+define("ROOT_PATH", ""); // https://www.yourdomain.com/ or https://www.yourdomain.com/website/
 
 ?>
