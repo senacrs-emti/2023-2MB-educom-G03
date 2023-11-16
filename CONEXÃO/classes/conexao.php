9ff15d7ce49dfbdad9a44b09aabca5397fc18287
halls <108465597+nathaliaalencastro@users.noreply.github.com>
2023-11-16 09:38:57 -0300

    Update conexao.php

--- CONEXÃO/classes/conexao.php
+++ CONEXÃO/classes/conexao.php
@@ -2,10 +2,10 @@
 
 $conexao = mysqli_connect('localhost','root','','leituras');
 // Define DB Params
-define("DB_HOST", ""); // ex. localhost
+define("DB_HOST", "localhost:8080"); // ex. localhost
 define("DB_USER", "");
 define("DB_PASS", "");
-define("DB_NAME", "");
+define("DB_NAME", "leituras");
 // Define URL
 define("ROOT_PATH", ""); 
 ?>
