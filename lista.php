f67358e5a90fed39eeb2df54b5b94269c4479017
Nathalia Alencastro <108465597+nathaliaalencastro@users.noreply.github.com>
2023-12-02 09:28:14 -0300

    Update lista.php

--- lista.php
+++ lista.php
@@ -12,17 +12,14 @@ if (!$conn) {
 
 $ano = $_GET['ano'];
 $sql = "SELECT * FROM livros WHERE edicao LIKE '%$ano%'";
-<<<<<<< HEAD
 $sqlgen = ";";
 $sqleditora = "SELECT * FROM editora ORDER BY '%$sql%'";
 
 $sqlgen = "SELECT nome FROM genero";
 $sqleditora = "SELECT nome FROM editora";
- 
-=======
 $sqlgen = "SELECT nome FROM genero";
 $sqleditora = "SELECT nome FROM editora";
->>>>>>> parent of 3145f6a (lista)
+
 
 ?>
 
@@ -30,6 +27,7 @@ $sqleditora = "SELECT nome FROM editora";
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Leituras Obrigatórias</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
@@ -55,23 +53,16 @@ $sqleditora = "SELECT nome FROM editora";
         <h1><?php echo $ano;?></h1>
 <?php
 $result = mysqli_query($conn, $sql);
-<<<<<<< HEAD
+
 
 while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) 
 while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) 
-=======
-while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
->>>>>>> parent of 3145f6a (lista)
+while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) 
 $result2 = mysqli_query($conn, $sqlgen);
 if ($row2 = mysqli_fetch_array($result2, MYSQLI_ASSOC)) 
 $result3 = mysqli_query($conn, $sqleditora);
-<<<<<<< HEAD
 if ($row3 = mysqli_fetch_array($result3, MYSQLI_ASSOC)) 
-while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-
-=======
-if ($row3 = mysqli_fetch_array($result3, MYSQLI_ASSOC)) {
->>>>>>> parent of 3145f6a (lista)
+while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) 
 ?>
       <div class="card1">
         <div class="card mb-3" style="max-width: 1180px;">
